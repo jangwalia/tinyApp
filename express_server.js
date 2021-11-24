53769913460ec89001c15c08ad369f7b6625f0cd
jang walia <jangiwalia83@gmail.com>
2021-11-24 09:40:24 -0800

    Display /urls route thorugh passing data to template files

--- express_server.js
+++ express_server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/urls",(req,res)=>{
+  res.render('urls_index',{urls : urlDatabase});
+})
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
