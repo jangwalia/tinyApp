fea3fff3a5cbd112c2dd4fc4201c7c7fc3bbb51a
jang walia <jangiwalia83@gmail.com>
2021-11-24 12:55:20 -0800

    implememted POST request from form back to urls page

--- express_server.js
+++ express_server.js
@@ -3,6 +3,19 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Generate random string
+function generateRandomString() {
+  let code = "";
+  const characters = '012345678910abcdefghijklmnopqrstuvwxyz';
+  for(let i = 0;i <= 6 ;i ++){
+  code +=   characters.charAt(Math.floor(Math.random() * characters.length));
+  }return code;
+  
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,6 +32,18 @@ app.get("/", (req, res) => {
 app.get("/urls",(req,res)=>{
   res.render('urls_index',{urls : urlDatabase});
 })
+//###### GET REQUEST TO SHOW THE FORM
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//######### POST REQUEST FROM THE FORM
+ app.post('/urls',(req,res)=>{
+  const {longURL} = req.body;
+  const newCode = generateRandomString();
+  urlDatabase[newCode] = longURL;
+  res.redirect('/urls');
+ })
 
 app.get('/urls/:shortURL',(req,res)=>{
   const{shortURL} = req.params;
@@ -29,6 +54,8 @@ app.get('/urls/:shortURL',(req,res)=>{
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
