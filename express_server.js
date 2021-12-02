2ff6d325a9327d31c25d4e67bddd310a5f7ead79
jang walia <jangiwalia83@gmail.com>
2021-12-02 11:53:55 -0800

    Refactored Update and Delete routes implementing user authenticatiob

--- express_server.js
+++ express_server.js
@@ -44,8 +44,10 @@ function checkemail(email,database){
 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  // {shorturl : {longUrL :  'www.some.com' , userID : user1randomID }}
+  //{shorturl 2 :  {{longUrL :  'www.abc.com' , userID : user2randomID }}}
+  //{shorturl 3 :  {{longUrL :  'www.xyz.com' , userID : user2randomID }}}
+  
 };
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'));
@@ -55,36 +57,51 @@ app.set('views',path.join(__dirname,'/views'));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+//function to get urls for user
+
+
+
+
 
 //show all urls route
 app.get("/urls",(req,res)=>{
+  let result = {};
   const {id} = req.cookies;
-  const user = users[id]
+  const user = users[id];
+  //to check if only currently logged user can access his created URLS
+  for(const url in urlDatabase){
+   if(urlDatabase[url].userID === id){
+    result[url] =  urlDatabase[url]
+   }
+  }
   const tempVariable = {
-   urls : urlDatabase,
-   user,
-}
+    user,
+    urls : result
+   }
   res.render('urls_index',tempVariable);
-})
+});
 //###### GET REQUEST TO SHOW THE FORM
 app.get("/urls/new", (req, res) => {
   const {id} = req.cookies;
   const user = users[id]
+  if(id){
   const tempVariable = {
    urls : urlDatabase,
    user,
 }
  res.render("urls_new",tempVariable);
+}else{
+  res.redirect('/login');
+}
 });
 
 //show individual url route
 app.get('/urls/:shortURL',(req,res)=>{
   const shortURL = req.params.shortURL;
-  const longurl = urlDatabase[shortURL];
+  const longurl = urlDatabase[shortURL]['longURL'];
   const {id} = req.cookies;
   const user = users[id];
   const tempVariable = {
-   urls : urlDatabase,
    user,
    longurl,
    shortURL
@@ -93,17 +110,24 @@ app.get('/urls/:shortURL',(req,res)=>{
 })
 //######### POST REQUEST FROM THE FORM
  app.post('/urls',(req,res)=>{
+  const {id} = req.cookies;
   const{longURL} = req.body;
-  const newCode = generateRandomString();
-  urlDatabase[newCode] = longURL;
-  res.redirect(`/urls/${newCode}`);
+  const shortURL = generateRandomString();
+  const data = {
+    longURL,
+    userID : id
+  }
+  urlDatabase[shortURL] = data;
+ 
+  
+  res.redirect(`/urls/${shortURL}`);
  })
 
 // fetch long url route
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   if(shortURL in urlDatabase){
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL]['longURL'];
   res.redirect(longURL);
   }
   else{
@@ -113,16 +137,35 @@ app.get("/u/:shortURL", (req, res) => {
 
 // Delete Route
 app.post('/urls/:shortURL/delete',(req,res)=>{
-  const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect('/urls');
+  const userid = req.cookies.id;
+  let currentUser = "";
+  const {shortURL} = req.params;
+  //only the current logged user can delete their urls
+  for(const key in urlDatabase){
+    currentUser = urlDatabase[key].userID;
+    if(currentUser !== userid){
+      res.redirect ('/login');
+    }
+    delete urlDatabase[shortURL];
+  }
+    res.redirect('/urls');
 })
 //UPDATE Route
-app.post('/urls/:id',(req,res)=>{
+ app.post('/urls/:id',(req,res)=>{
+  let currentUser = "";
+  const userid = req.cookies.id;
   const id = req.params.id;
-  const longURL = req.body.newURL
-  urlDatabase[id] = longURL;
+  //To check that only logged user can edit their urls
+  for(const user in urlDatabase){
+     currentUser = urlDatabase[user].userID;
+     if(currentUser !== userid){
+      res.redirect('/login');
+    }
+    let newURL = req.body.newURL;
+    urlDatabase[user].longURL = newURL;
+  }
   res.redirect('/urls');
+
 })
 //Log IN Route
 
